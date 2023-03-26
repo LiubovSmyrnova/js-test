fcd878d97894bc52b309ecf43c685098362f4123
Liubov Smyrnova <122973484+LiubovSmyrnova@users.noreply.github.com>
2023-03-26 10:32:51 +0200

    update

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function logFunction () {
     console.log ("This is a function!")
 }
+//define the function
 logFunction ();
+//calling for the function to execute
 
 function describeSelf (name, age, favouriteFood) {
 const personsinfo = `My name is ${name} and I'm ${age} years old. My favourite food is ${favouriteFood}.`
 return personsinfo;
 }
-describeSelf (anna, 30, pizza);
-const infoAnna = describeSelf (Anna, 30, pizza);
 
+const infoAnna = describeSelf ("Anna", 30, "pizza");
 
-//describeSelf (Gustav, 28, pasta);
-//const infoGustav = describeSelf (Gustav, 28, pasta);
-//describeSelf (Lena, 59, tiramisu);
-//const infoLena = (Lena, 59, tiramisu);
+const infoGustav = describeSelf ("Gustav", 28,"pasta");
+
+const infoLena = describeSelf ("Lena", 59, "tiramisu");
 
 console.log (infoAnna);
-//console.log (infoGustav);
-//console.log (infoLena);
+console.log (infoGustav);
+console.log (infoLena);
 
